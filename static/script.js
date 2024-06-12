40b35cce641313365ce8fd3216ee408ee08f426a
Victor Ferrer <ferrer.morgado@gmail.com>
2024-06-12 19:07:54 -0300

    added skills progress bars, sidebar selection js still not done

--- static/script.js
+++ static/script.js
@@ -9,3 +9,13 @@ menuMobile.addEventListener('click', () => {
     menuMobile.classList.replace('bi-x', 'bi-list');
     body.classList.toggle('menu-nav-active');
 })
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        menuMobile.classList.replace('bi-x', 'bi-list');
+        item.classList.add('active');
+        body.classList.toggle('menu-nav-active');
+    })
+})
